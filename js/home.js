2f292ba77169d443765ed80a67dfba6b4f36835a
SiemenGijbels <gijbelssiemen@gmail.com>
2015-05-27 23:19:28 +0200

    uydef

--- js/home.js
+++ js/home.js
@@ -19,7 +19,7 @@ $(function  () {
 			var htmlString = "";
 
 			htmlString += '<header class="color'+val.color_code+'"><a href="" class="info" id="'+val.id+'">';
-			htmlString += '<img src="https://d13yacurqjgara.cloudfront.net/users/180760/avatars/normal/TEC---Dribble-Avatar-Red-01.png">';
+			htmlString += '<img src="https://d13yacurqjgara.cloudfront.net/users/761348/avatars/normal/879463c21f43696a38c83a2e29034b0e.png?1424166156">';
 			htmlString += '<div><h1>'+val.title+'</h1><h2>'+val.location+'</h2></div><span class="detail"></span>';
 			htmlString += '</a></header>';
 			htmlString += '<aside><header class="hide"><h1>vacancie options</h1></header>';
